c8eb0f647a1d31dc26341c7ed57783b91c9b04ed
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-09-13 01:42:17 -0700

    some css is add to index.css related to searchBox

--- src/Input.jsx
+++ src/Input.jsx
@@ -7,13 +7,15 @@ const Input = ({ handleChange, hint }) => {
 
       <label htmlFor="input">{hint[0]}</label>
       <input type="text" id="input" onChange={handleChange} />
-      <div className="searchBox">
-  {hint.length > 0 && (<ul>
+      {hint.length > 0 && ( <div className="searchBox">
+ 
+ <ul>
       {hint.map((city,index)=>(
          <li key={index}>{city}</li>
       ))}
-   </ul>)}
-      </div>
+   </ul>
+   </div>)}
+     
     </div>
   );
 };
