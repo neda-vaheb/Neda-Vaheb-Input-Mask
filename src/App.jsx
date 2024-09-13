2c111ecf854dadf0dd87a5d82658fb0249e2a2e9
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-09-12 20:08:38 -0700

    App.jsx and Input.jsx are complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react";
 
+import cities from "./cities.json";
+import Input from "./Input";
 
 function App() {
   
 
+  const [inputValue, setInputValue] = useState("");
+  const [hint, setHint] = useState([]);
+
+  const handelChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    if (value) {
+      const filteredCities = cities.filter((city) => city.startsWith(value));
+      setHint(filteredCities);
+    } else {
+      setHint([]);
+    }
+  };
   return (
     <>
-     
+      <h1>Search The City</h1>
+
+      <Input
+        handleChange={handelChange}
+        hint={hint}
+        inputValue={inputValue}
+      />
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/Input.jsx
@@ -0,0 +1,26 @@
+import { TbWorldSearch } from "react-icons/tb";
+
+const Input= ({handleChange, hint}) => {
+  
+  return (
+    <div className="input">
+<TbWorldSearch className="icon" />
+
+      <label htmlFor="input" >
+   {hint[0]}
+      </label>
+      <input
+        type="text"
+        id="input"
+        onChange={handleChange}
+      />
+        {/* {hint.length > 0 && (<ul>
+      {hint.map((city,index)=>(
+         <li key={index}>{city}</li>
+      ))}
+   </ul>)} */}
+    </div>
+  );
+};
+
+export default Input;
